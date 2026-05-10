785e9fd47c5428f7165a4d8b655f65d6fd5cbf6b
Rikokyu <nhannt@sosene.asia>
2026-05-10 09:46:27 +0700

    Add animation and movement for Player

--- Assets/Scripts/PlayerRideMovement.cs
+++ Assets/Scripts/PlayerRideMovement.cs
@@ -9,6 +9,8 @@ public class PlayerRideMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
+    public bool playingRideSound = false;
+    public float rideSoundSpeed = 0.5f;
 
     void Start()
     {
@@ -16,9 +18,25 @@ public class PlayerRideMovement : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
+        if(PauseController.IsGamePaused)
+        {
+            rb.linearVelocity = Vector2.zero;
+            animator.SetBool("isRiding", false);
+            StopRideSound();
+            return;
+        }
         rb.linearVelocity = moveInput * moveSpeed;
+        animator.SetBool("isRiding", rb.linearVelocity.magnitude > 0);
+        if(rb.linearVelocity.magnitude > 0 && !playingRideSound)
+        {
+            StartRideSound();
+        }
+        else if(rb.linearVelocity.magnitude == 0)
+        {
+            StopRideSound();
+        }
     }
     public void Move(InputAction.CallbackContext context)
     {
@@ -35,4 +53,19 @@ public class PlayerRideMovement : MonoBehaviour
         animator.SetFloat("InputX", moveInput.x);
         animator.SetFloat("InputY", moveInput.y);
     }
+        void StartRideSound()
+    {
+        playingRideSound = true;
+        InvokeRepeating(nameof(PlayRideSound), 0f, rideSoundSpeed);
+    }
+    void StopRideSound()
+    {
+        playingRideSound = false;
+        CancelInvoke(nameof(PlayRideSound));
+    }
+    void PlayRideSound()
+    {
+        SoundEffectManager.Play("RideSound", true); 
+    }
+
 }
