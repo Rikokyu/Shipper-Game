fb8152234ac5a28188e4eab52409cb0c3702df5c
Rikokyu <nhannt@sosene.asia>
2026-05-11 19:28:45 +0700

    fix script Player Movement, add UIShipper, fix animation

---
+++ Assets/Scripts/PlayerItemCollector.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerItemCollector : MonoBehaviour
+{
+    private InventoryController inventoryController;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        inventoryController = FindObjectOfType<InventoryController> ();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            Item item = collision.GetComponent<Item>();
+            if(item != null)
+            {
+                //Add item to inventory
+                bool itemAdded = inventoryController.AddItem(collision.gameObject);
+                if (itemAdded)
+                {
+                    Destroy(collision.gameObject);
+                }
+
+            }
+        }
+    }
+}
