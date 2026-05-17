7b86f8cc4709a1b6ff14e40209ab9d3c7f21f2a9
Rikokyu <nhannt@sosene.asia>
2026-05-17 12:56:18 +0700

    Done temp project before report

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public static DialogueController Instance { get; private set; }
+
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+    public Transform choiceContainer;
+    public GameObject choiceButtonPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject); //Make sure only one instance
+    }
+    public void ShowDialogueUI(bool show)
+    {
+        dialoguePanel.SetActive(show); //Toggle UI visability
+    }
+    public void SetNPCInfo(string npcName, Sprite portrait)
+    {
+        nameText.text = npcName;
+        portraitImage.sprite = portrait;
+    }
+    public void SetDialogueText(string text)
+    {
+        dialogueText.text = text;
+    }
+    public void ClearChoices()
+    {
+        foreach (Transform child in choiceContainer) Destroy(child.gameObject);
+    }
+    public void CreateChoiceButton(string choiceText, UnityEngine.Events.UnityAction onclick)
+    {
+        GameObject choiceButton = Instantiate(choiceButtonPrefab, choiceContainer);
+        choiceButton.GetComponentInChildren<TMP_Text>().text = choiceText;
+        choiceButton.GetComponent<Button>().onClick.AddListener(onclick);
+        // return choiceButton;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
+    public bool playingFootSteps = false;
+    public float footStepsSpeed = 0.5f;
 
     void Start()
     {
@@ -16,15 +18,28 @@ public class PlayerMovement : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
+        if(PauseController.IsGamePaused)
+        {
+            rb.linearVelocity = Vector2.zero;
+            animator.SetBool("isWalking", false);
+            StopFootsteps();
+            return;
+        }
         rb.linearVelocity = moveInput * moveSpeed;
+        animator.SetBool("isWalking", rb.linearVelocity.magnitude > 0);
+        if(rb.linearVelocity.magnitude > 0 && !playingFootSteps)
+        {
+            StartFootsteps();
+        }
+        else if(rb.linearVelocity.magnitude == 0)
+        {
+            StopFootsteps();
+        }
     }
     public void Move(InputAction.CallbackContext context)
     {
-        animator.SetBool("isWalking", true);
-
-
         if (context.canceled)
         {
             animator.SetBool("isWalking", false);
@@ -34,5 +49,19 @@ public class PlayerMovement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
         animator.SetFloat("InputX", moveInput.x);
         animator.SetFloat("InputY", moveInput.y);
+    }
+    void StartFootsteps()
+    {
+        playingFootSteps = true;
+        InvokeRepeating(nameof(PlayFootStep), 0f, footStepsSpeed);
+    }
+    void StopFootsteps()
+    {
+        playingFootSteps = false;
+        CancelInvoke(nameof(PlayFootStep));
+    }
+    void PlayFootStep()
+    {
+        SoundEffectManager.Play("FootStep", true); 
     }
 }
